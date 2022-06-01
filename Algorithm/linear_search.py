59080ea2ac89e8a57e8a46c9f8e4191aaeb314f3
tententgc <nyman3527@gmail.com>
2022-06-01 20:51:53 +0700

    Add datastructure

--- Algorithm/linear_search.py
+++ Algorithm/linear_search.py
@@ -1,13 +1,12 @@
 def linear_search(list, target):
     """
-    Returns the index position of the target if found,else returns None  
+    Returns the index position of the target if found,else returns None
     """
-
+    
     for i in range(0, len(list)):
         if list[i] == target:
             return i
     return None
-
 
 def verify(index):
     if index is not None:
